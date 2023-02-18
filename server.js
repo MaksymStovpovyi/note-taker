0cdcbb40be2c19713f7206251d8f33d301bac5eb
maxSt <stovpovymax@gmail.com>
2023-02-18 13:20:17 -0500

    added middleware

--- server.js
+++ server.js
@@ -1,18 +1,43 @@
-import express from 'express'
-import path from 'path'
-const __dirname = path.resolve()
+// added libraries
+const express = require('express'); 
+const path = require('path');
+const fs = require('fs');
 
-const PORT = process.env.PORT ?? 3001
-const app = express()
+// created the port for server
+const PORT = process.env.PORT ?? 3001;
 
-app.get('/*', (req, res) => {
+// created the app from express
+const app = express();
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+
+
+
+
+
+
+
+
+
+// get to  '/'
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-})
-
+});
+// get to  '/notes'
 app.get('/notes', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'notes.html'));
-})
+});
+// get to another pages
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
+// start server
 app.listen(PORT, () => {
-    console.log(`start server ${PORT} ^_^ ...`)
-})
+    console.log(`start server ${PORT} ^_^ ...`);
+});

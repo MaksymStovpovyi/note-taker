9ab30c3b155f4ac5625a1a9ab16df1cb3d918836
maxSt <stovpovymax@gmail.com>
2023-02-18 22:35:10 -0500

    added db and idDb

--- server.js
+++ server.js
@@ -9,20 +9,28 @@ const PORT = process.env.PORT ?? 3001;
 // created the app from express
 const app = express();
 
-//middleware
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// pull the db
+let notes = require('./db/db.json') || [];
+// create id
+let dbId = notes[0] || 0;
+if (notes.length === 0) {
+    notes.push(dbId)
+};
 
+// write to db.js
+function writeFile(path_=path.join(__dirname, './db/db.json'), data=notes){
+    fs.writeFileSync(path_, JSON.stringify(data, null, 2));
+}
 
-
-
-
-
-
-
-
+// array from db
+app.get('/api/notes', (req, res) => {
+    res.json(notes.slice(1));
+});
 
 // get to  '/'
 app.get('/', (req, res) => {
@@ -37,7 +45,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
-// start server
+// start the server
 app.listen(PORT, () => {
     console.log(`start server ${PORT} ^_^ ...`);
 });
